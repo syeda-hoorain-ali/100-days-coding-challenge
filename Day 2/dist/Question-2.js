7a56456260f9ef05600c3592f0f45ab2aaa7bdf3
Syeda Hoorain Ali <syedahoorainali8gmail.com>
2024-03-30 18:35:23 +0500

    remove extra files

--- Day 2/dist/Question-2.js
+++
@@ -1,4 +0,0 @@
-// Question 2: Personal Message: Store a person’s name in a variable and print a message to them, like “Hello Hoorain, would you like to learn some TypeScript today?”
-const personName = "Hoorain";
-console.log(`Hello ${personName}, would you like to learn some TypeScript today?`);
-export {};
