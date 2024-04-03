3ba6ab6cc9171c127b0bec7180ba8fac7f4fd061
Syeda Hoorain Ali <syedahoorainali8gmail.com>
2024-04-03 13:36:28 +0500

    added day 25

---
+++ Day 25/dist/Question-73.js
@@ -0,0 +1,9 @@
+// Question 73: Assigning and Updating Variables: Create a function where you declare a variable using let and assign an initial value. Then, update its value within the same function and log both the initial and updated values.
+const makeVariable = () => {
+    let name = "Hoorain";
+    console.log("Initial value:", name);
+    name = "Mantasha";
+    console.log("Updated value:", name);
+};
+makeVariable();
+export {};
---
+++ Day 25/dist/Question-74.js
@@ -0,0 +1,12 @@
+// Question 74: Swapping Variables: Demonstrate how to swap the values of two variables. Start with variables a = 5 and b = 10, then swap their values so that a becomes 10 and b becomes 5.
+const swapValues = () => {
+    let a = 5;
+    let b = 10;
+    console.log(`Before swap: a = ${a} b = ${b}`);
+    let c = a; //c = 5
+    a = b; // a = 10
+    b = c; // b = 5
+    console.log(`After swap: a = ${a} b = ${b}`);
+};
+swapValues();
+export {};
