671d960441a0e3fceedd2fcce55af360a9c33798
Syeda Hoorain Ali <syedahoorainali8gmail.com>
2024-04-09 16:08:16 +0500

    added day 39

---
+++ Day 39/dist/Question-116.js
@@ -0,0 +1,30 @@
+// Question 116: Create a switch case that matches several cases to the same code block, representing seasons.
+const printSeasons = (month) => {
+    switch (month) {
+        case 12:
+        case 1:
+        case 2:
+            console.log("Winter");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            console.log("Spring");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            console.log("Summer");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            console.log("Autumn");
+            break;
+        default:
+            console.log("Invalid month");
+            break;
+    }
+};
+printSeasons(4);
+export {};
---
+++ Day 39/dist/Question-117.js
@@ -0,0 +1,18 @@
+// Question 117: Implement a switch statement that evaluates an expression and uses the default case if none of the cases match.
+const printFruits = (fruit) => {
+    switch (fruit.toLowerCase()) {
+        case "apple":
+            console.log("It's an apple.");
+            break;
+        case "orange":
+            console.log("It's an orange.");
+            break;
+        case "banana":
+            console.log("It's a banana.");
+            break;
+        default:
+            console.log("It's something else.");
+    }
+};
+printFruits("kiwi");
+export {};
