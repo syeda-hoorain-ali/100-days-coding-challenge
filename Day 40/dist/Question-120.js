6858acb684d09a893baa0312b9ef85c68ea0a074
Syeda Hoorain Ali <syedahoorainali8gmail.com>
2024-04-10 03:09:02 +0500

    added day 40

---
+++ Day 40/dist/Question-120.js
@@ -0,0 +1,12 @@
+// Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+const favoriteMovies = [
+    "The Shawshank Redemption",
+    "The Godfather",
+    "The Dark Knight",
+    "Pulp Fiction",
+    "Forrest Gump"
+];
+for (const movie of favoriteMovies) {
+    console.log(movie);
+}
+export {};
