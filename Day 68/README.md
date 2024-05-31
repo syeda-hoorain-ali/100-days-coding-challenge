d2f7b89bf95b60c9624784a8884cffffe0930ce6
Syeda Hoorain Ali <syedahoorainali8gmail.com>
2024-05-31 17:42:29 +0500

    added day 68

---
+++ Day 68/README.md
@@ -0,0 +1,18 @@
+## 🚀 Day 68 Challenge: TypeScript Tuples 🚀
+
+## Contents
+- [Tuple Data Type in TypeScript](#tuple-data-type-in-typescript)
+
+## Tuple Data Type in TypeScript
+
+TypeScript tuples are arrays where the type of each element is known. They are fixed in length and their elements can be of different types.
+
+### Tasks
+1. **Declare a Tuple:** Declare a tuple variable in TypeScript with at least three elements, each of a different type. Access and print each element of the tuple.
+2. **Changing Values:** Try to update value of a tuple. Verify the changes by logging the tuple.
+3. **Tuple Destructuring:** Use tuple destructuring to extract values from a tuple into separate variables. Perform operations with these variables and print the results.
+
+   
+## References
+- [Tuple Data Type in TypeScript](https://github.com/AsharibAli/100-days-of-code/tree/main/day-68/TS-Tuple)
+---
---
+++ Day 68/dist/index.js
@@ -0,0 +1,31 @@
+"use strict";
+//******************
+//*     Task 1     *
+//******************
+// Declare a tuple variable
+let myTuple = [10, "Hello", true];
+// Access and print each element of the tuple
+console.log("First element:", myTuple[0]);
+console.log("Second element:", myTuple[1]);
+console.log("Third element:", myTuple[2]);
+//******************
+//*     Task 2     *
+//******************
+// Update an element in the tuple
+// myTuple[0] = 20; //! Error: Cannot assign to '0' because it is a read-only property.
+console.log("After updating:", myTuple);
+// Update whole array
+myTuple = [10, "Hello", true];
+//******************
+//*     Task 3     *
+//******************
+// Tuple destructuring
+let [num, str, bool] = myTuple;
+// Perform operations with the extracted values
+let doubledNum = num * 2;
+let uppercaseStr = str.toUpperCase();
+let negatedBool = !bool;
+// Print the results
+console.log("Doubled number:", doubledNum);
+console.log("Uppercase string:", uppercaseStr);
+console.log("Negated boolean:", negatedBool);
